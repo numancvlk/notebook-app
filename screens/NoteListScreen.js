321cbd4032c6e02d67712c3a4b55271dbf006cdd
Numan Civelek <nmn.cvlk@gmail.com>
2025-06-10 14:55:56 +0300

    Arama çubuğu eklendi

--- screens/NoteListScreen.js
+++ screens/NoteListScreen.js
@@ -5,7 +5,10 @@ import {
   FlatList,
   StyleSheet,
   Dimensions,
+  TextInput,
 } from "react-native";
+
+import { useState } from "react";
 
 //---------COMPONENTS-------------
 import NoteItem from "../components/NoteItem";
@@ -15,6 +18,15 @@ const { width } = Dimensions.get("window");
 const itemWidth = (width - 60) / 2;
 
 export default function NoteListScreen({ navigation, notes, onDeleteNote }) {
+  const [search, setSearch] = useState("");
+
+  const filteredNotes = notes.filter((note) => {
+    return (
+      note.title.toLowerCase().includes(search.toLowerCase()) ||
+      note.text.toLowerCase().includes(search.toLowerCase())
+    );
+  });
+
   const handleAddNotePress = () => {
     navigation.navigate("NoteDetailScreen");
   };
@@ -41,6 +53,14 @@ export default function NoteListScreen({ navigation, notes, onDeleteNote }) {
   return (
     <View style={myStyles.container}>
       <Header />
+
+      <TextInput
+        style={myStyles.searchBar}
+        value={search}
+        onChangeText={setSearch}
+        placeholder="Search notes.."
+      />
+
       {notes.length === 0 ? (
         <View style={myStyles.emptyNotesContainer}>
           <Text style={myStyles.emptyNotesText}>Henüz Notunuz Yok</Text>
@@ -50,7 +70,7 @@ export default function NoteListScreen({ navigation, notes, onDeleteNote }) {
           numColumns={2}
           columnWrapperStyle={myStyles.row}
           contentContainerStyle={myStyles.flatListContent}
-          data={notes}
+          data={filteredNotes}
           keyExtractor={(item) => item.id}
           renderItem={renderNoteItem}
         />
@@ -66,16 +86,27 @@ export default function NoteListScreen({ navigation, notes, onDeleteNote }) {
 const myStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5", // Açık gri arka plan
-    paddingTop: 0, // Header bileşeni kendi padding'ini yönetecek
+    backgroundColor: "#f5f5f5",
+    paddingTop: 0,
+  },
+  searchBar: {
+    padding: 12,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    padding: 12,
+    marginBottom: 15,
+    backgroundColor: "#fff",
+    fontSize: 16,
   },
   flatListContent: {
-    paddingHorizontal: 15, // FlatList'in yanlardan iç boşluğu (itemWidth hesaplamasına göre ayarlandı)
-    paddingVertical: 10, // Üstten ve alttan boşluk
+    paddingHorizontal: 15,
+    paddingVertical: 10,
   },
   row: {
-    justifyContent: "space-between", // Not kartlarını yatayda eşit boşluklarla dağıt
-    marginBottom: 10, // Satırlar arası boşluk
+    justifyContent: "space-between",
+    marginBottom: 10,
   },
   emptyNotesContainer: {
     flex: 1,
@@ -89,17 +120,17 @@ const myStyles = StyleSheet.create({
     textAlign: "center",
   },
   fab: {
-    position: "absolute", // Butonu ekranda sabitle
+    position: "absolute",
     width: 60,
     height: 60,
     alignItems: "center",
     justifyContent: "center",
     right: 30,
     bottom: 30,
-    backgroundColor: "#007bff", // Mavi renk
-    borderRadius: 30, // Yuvarlak yapar
-    elevation: 8, // Android gölge
-    shadowColor: "#000", // iOS gölge
+    backgroundColor: "#007bff",
+    borderRadius: 30,
+    elevation: 8,
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 4,
